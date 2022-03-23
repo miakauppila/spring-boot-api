99c9178b4a1701c50e8d93804b9d88e00cf7f8df
miakauppila <mia.kauppila@gmail.com>
2022-03-23 21:32:17 +0200

    Add POST request method

--- demo/src/main/java/com/example/demo/student/StudentRepository.java
+++ demo/src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,8 +1,19 @@
 package com.example.demo.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
+
+    //JPQL query
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+
+    // SQL alternative
+    // SELECT * FROM student WHERE email = ?
+    
+    Optional<Student> findStudentByEmail(String email);
 }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,13 @@ public class StudentService {
 	public List<Student> getStudents() {
 		return studentRepository.findAll();
 	}
+
+    public void addNewStudent(Student student) {
+		Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+		if(studentOptional.isPresent()) {
+			throw new IllegalStateException("Email taken");
+		}
+		studentRepository.save(student);
+    }
     
 }
